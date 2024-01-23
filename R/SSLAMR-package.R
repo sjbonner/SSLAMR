9ac3629752c5f127dace414a88ede910c262419d
Simon Bonner <sbonner6@uwo.ca>
2024-01-23 09:48:14 -0500

    Added missing imports of functions from stats and utils.

--- R/SSLAMR-package.R
+++ R/SSLAMR-package.R
@@ -7,4 +7,17 @@
 #' @import rjags
 #' @import readxl
 #' @importFrom glmnet glmnet
+#' @importFrom stats glm
+#' @importFrom stats median
+#' @importFrom stats pchisq
+#' @importFrom stats poisson
+#' @importFrom stats qt
+#' @importFrom stats quantile
+#' @importFrom stats rbinom
+#' @importFrom stats rnorm
+#' @importFrom stats rpois
+#' @importFrom stats runif
+#' @importFrom stats sd
+#' @importFrom utils setTxtProgressBar
+#' @importFrom utils txtProgressBar
 NULL
