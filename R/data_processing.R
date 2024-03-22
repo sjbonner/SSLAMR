a173d0aa69fb1d6bc68f004e993a9dc4ad401abf
sjbonner <sbonner6@uwo.ca>
2024-03-22 15:49:28 -0400

    Added new function create_bins_2(). This function defines bins based on the peaks in the spectrum in order to match isotopes with the nearest neighbouring peak (if its within epsilon).
    Renamed create_bins() to create_bins_1().

--- R/data_processing.R
+++ R/data_processing.R
@@ -59,9 +59,9 @@ candidate_info <- function(candidates,
     filter(Mass >= min_mass_charge, Mass <= max_mass_charge)
 }
 
-# lipid bins: defines the boundaries of the bins for creating
-# the design matrix
-create_bins <- function(isotope_df, epsilon = .05, min_mass_charge = 0, max_mass_charge = 1000){
+
+create_bins_1 <- function(isotope_df, epsilon = .05, min_mass_charge = 0, max_mass_charge = 1000){
+  ## Defines bins based on the candidates and their isotopes
   
   bins1 <- tibble(Mass = sort(unique(pull(isotope_df,"Mass")))) %>%
     mutate(Delta = Mass - lag(Mass, default = 0),
@@ -86,6 +86,32 @@ create_bins <- function(isotope_df, epsilon = .05, min_mass_charge = 0, max_mass
     mutate(Width = Upper - Lower)
 }
 
+create_bins_2 <- function(spectrum, epsilon, min_mass_charge, max_mass_charge){
+  
+  ## Defines bins based on the peaks in the spectrum
+  
+  mids <- (head(spectrum$`Mass/Charge`,-1) + tail(spectrum$`Mass/Charge`,-1))/2
+  
+  intervals1 <- spectrum |> 
+    mutate(Lower = pmax(`Mass/Charge`-epsilon,c(-Inf,mids)),
+           Upper = pmin(`Mass/Charge`+epsilon,c(mids,Inf)),
+           Width = Upper - Lower) |> 
+    select(Lower, Upper, Width)
+  
+  intervals2 <- tibble(Lower = head(intervals1$Upper,-1),
+                       Upper = tail(intervals1$Lower,-1),
+                       Width = Upper - Lower) |> 
+    filter(Width > 0)
+  
+  intervals <- intervals1 |> 
+    bind_rows(intervals2) |> 
+    bind_rows(tibble(Lower = c(min_mass_charge,max(intervals1$Upper)),
+                     Upper = c(min(intervals1$Lower),max_mass_charge),
+                     Width = Upper - Lower)) |> 
+    arrange(Lower) |> 
+    mutate(Mass = (Lower + Upper)/2) |> 
+    rowid_to_column("Interval")
+}
 # this function takes the output from lipids_info and 
 # creates the design matrix
 build_design <- function(candidate_bins, epsilon=0.05){
@@ -145,7 +171,8 @@ sslamr_data <- function(spectrum,
                         min_abundance = .001,
                         max_isotopes = Inf,
                         skip_isotopes = 0,
-                        epsilon=0.05, 
+                        epsilon=0.05,
+                        binning = TRUE,
                         min_mass_charge = NULL,
                         max_mass_charge = NULL,
                         ran.seed=unclass(Sys.time()),
@@ -179,10 +206,19 @@ sslamr_data <- function(spectrum,
   # 2) Define bins
   if(verbose)
     message("  Defining bins...")
-  intervals <- create_bins(isotope_data, 
-                           epsilon, 
-                           min_mass_charge = min_mass_charge,
-                           max_mass_charge = max_mass_charge)
+  
+  if(binning){
+    intervals <- create_bins_1(isotope_data, 
+                               epsilon, 
+                               min_mass_charge = min_mass_charge,
+                               max_mass_charge = max_mass_charge)
+  }
+  else{
+    intervals <- create_bins_2(spectrum,
+                               epsilon = epsilon, 
+                               min_mass_charge = min_mass_charge,
+                               max_mass_charge = max_mass_charge)
+  }
   
   # 3) Assign isotopes to bins
   candidate_bins <- isotope_data %>%
@@ -214,7 +250,7 @@ sslamr_data <- function(spectrum,
     full_join(counts, by = "Interval") %>%
     full_join(design, by = "Interval") %>%
     arrange(Interval) %>%
-    mutate(across(!c(Interval, Isotopes, Mass, Lower, Upper, Width, Peaks, Count), ~replace_na(.x, 0)))
+    mutate(across(!c(Interval, Mass, Lower, Upper, Width, Peaks, Count), ~replace_na(.x, 0)))
   
   # Return complete output
   list(candidates = candidate_bins,
