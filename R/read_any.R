001567f4385bd33215e6e21cbb8b134fcd45820d
Simon Bonner <sbonner6@uwo.ca>
2024-05-21 14:55:23 -0400

    Added documentation and ... .

--- R/read_any.R
+++ R/read_any.R
@@ -1,4 +1,17 @@
-read_any <- function(filename){
+#' Read data from files of (m)any format(s)
+#'
+#' Wrapper for the most common `read_*()` functions from the tidyverse and
+#' readxl packages that attempts to guess the file type based on the extension.
+#' Currently reads `.csv` as comma separated, `.txt` as tab delimited, and
+#' `.xlsx` as Excel spreadsheet. Any further arguments are passed directly to
+#' the corresponding function.
+#'
+#' @param filename String providing the path to the input file.
+#'
+#' @return Data frame created by importing the file.
+#' @export
+#'
+read_any <- function(filename, ...){
   ## Check that that the filename exists
   if(!file.exists(filename))
     stop("There is no file called ",filename,".\n")
@@ -17,5 +30,5 @@ read_any <- function(filename){
     stop("Sorry, I don't know how to read files of type ",ext,".\n")
     
   ## Read file
-  myreader(filename)
+  myreader(filename,...)
 }
