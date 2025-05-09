8acbf6de479745a439c925fb8ce821fd955b62ef
Simon Bonner <sbonner6@uwo.ca>
2025-05-09 14:07:24 -0400

    Added stand alone function for the greedy algorithm. This is much more transparent. Doesn't rely on fitting another model and groups a posteriori in the same way as the other models.

--- R/greedy.R
+++ R/greedy.R
@@ -1,7 +1,5 @@
 fit_single <- function(name, x, count){
   
-  #browser()
-  
   fit <- glmnet(cbind(1,x),
                 count,
                 family = poisson(link = "identity"),
@@ -27,9 +25,9 @@ fit_single <- function(name, x, count){
   # 3) P-value
   p_val <- pchisq(deviance, 1, lower.tail = FALSE)
   
-  tibble(abundance = coeffs[2], 
-         deviance = deviance, 
-         p = p_val)
+  tibble(Abundance = coeffs[2], 
+         Deviance = deviance, 
+         `p-value` = p_val)
 }
 
 greedy_fit <- function(data,
@@ -83,10 +81,10 @@ greedy_fit <- function(data,
       mutate(Total = sum(Count)) |> 
       summarize(fit_single(first(Name), Value, Count[Interval]),
                 .groups = "drop") |> 
-      filter(p < critical, abundance > 0) |>
-      arrange(p)
+      filter(Abundance > 0) |>
+      arrange(`p-value`)
 
-    if(any(fits$p < critical)){
+    if(fits$`p-value`[1] < critical){
       
       # Save fit information
       output <- output |> 
@@ -96,9 +94,10 @@ greedy_fit <- function(data,
       name <- fits |> 
         head(1) |> 
         pull("Name")
+      
       abund <- fits |> 
         head(1) |> 
-        pull("abundance")
+        pull("Abundance")
       
       tmp <- design1 |> 
         filter(Name == name)
@@ -116,6 +115,273 @@ greedy_fit <- function(data,
     }
   }
     
-  # Return
-  output
+  # Save output
+  write_xlsx(output, 
+             path = xlsx_out)
+}
+
+greedy_fit_2 <- function(spectrum = NULL,
+                         candidates = NULL,
+                         isotope_data = NULL,
+                         adducts = NULL,
+                         isoinfo = NULL,
+                         replace_isoinfo = FALSE,
+                         min_abundance = .001,
+                         group_formula = NULL,
+                         group_pattern= TRUE,
+                         pattern_tol = .05,
+                         binning = TRUE,
+                         epsilon = .05,
+                         rounding = "nearest",
+                         min_mass_charge = NULL,
+                         max_mass_charge = NULL,
+                         prescreen = 0,
+                         prescreen_prior = 1,
+                         prescreen_weight = FALSE,
+                         critical = .05,
+                         xlsx_out = NULL,
+                         verbose = TRUE){
+  
+  # Check input
+  if (is.null(spectrum))
+    stop("You must supply the spectrum data.")
+  
+  if(is.null(candidates) & is.null(isotope_data))
+    stop("You must supply one of either the list of candidates or the formatted isotope data.")
+  
+  if((!is.null(candidates) & !is.null(isotope_data)))
+    stop("You cannot supply both the list of candidates and the formatted isotope data.")
+  
+  if(!is.null(isotope_data) & !is.null(adducts))
+    stop("You cannot supply a list of adducts with already formatted isotope data.")
+  
+  # Save input arguments for writing to output
+  parameters <- mget(ls(environment(), sorted=F)) |> 
+    c(match.call(expand.dots=F)$...) |>
+    lapply(\(x) ifelse(is.null(x), "", x)) |> 
+    as_tibble() |> 
+    mutate(across(everything(), as.character)) |>
+    pivot_longer(everything(), names_to = "Argument", values_to = "Value")
+  
+  # Add SSLAMR version
+  parameters <- tibble(Argument = "SSLAMR Version", 
+                       Value = as.character(packageVersion("SSLAMR"))) |> 
+    bind_rows(parameters)
+  
+  # Read input files
+  if(verbose) message("Loading input data...")
+  
+  # 1) Spectrum data
+  if(is.character(spectrum)){
+    if(verbose) message("   Loading spectrum data...")
+    spectrum <- read_any(spectrum)
+  }  
+  
+  # 2) Candidate data
+  if(is.character(candidates)){
+    if(verbose) message("   Loading candidates data (",candidates,")")
+    candidates <- read_any(candidates)
+  }
+  
+  # 3) Isotope data
+  if(is.character(isotope_data)){
+    if(verbose) message("   Loading isotope data (",isotope_data,")")
+    isotope_data <- read_any(isotope_data)
+  }
+  
+  # 4) Adduct data
+  if(is.character(adducts)){
+    if(verbose) message("   Loading adducts data (",adducts,")")
+    adducts <- read_any(adducts)
+  }
+  
+  # 5) Element isotope information
+  if(is.character(isoinfo)){
+    if(verbose) message("   Loading element isotope information (",isoinfo,")")
+    isoinfo <- read_any(isoinfo)
+  }
+  
+  # Add element isotope information to ecipex's existing list
+  if(!replace_isoinfo)
+    isoinfo <- isoinfo %>%
+    bind_rows(ecipex::nistiso)
+  
+  # Add Prior column to candidate list if not specified
+  if(!("Prior" %in% colnames(candidates))){
+    candidates <- candidates %>%
+      add_column(Prior = NA)
+  }
+  
+  # Modify candidates via adducts
+  if(!is.null(adducts)){
+    candidates <- candidates %>%
+      modify_adducts(adducts)
+  }
+  else{
+    candidates <- candidates %>%
+      mutate(Parent = Name)
+  }
+  
+  if(verbose) message("Processing data...")
+  tic()
+  
+  # Group candidates with identical chemical formulas
+  if(!is.null(group_formula)){
+    # Group candidates has been set by user. Check setting is applicable.
+    if(group_formula & !is.null(adducts))
+      stop("Error: Candidates cannot be grouped when adducts are included. Please set group_formula to FALSE.\n")
+  }
+  else{
+    # Group candidates not set by user but not applicable.
+    if(!is.null(adducts)){
+      if(verbose) message("   Adducts provided. Not grouping candidates...")
+      group_formula <- FALSE
+    }
+    else if(any(!is.na(candidates$Prior))){
+      if(verbose) message("   Adducts provided. Not grouping candidates...")
+      group_formula <- FALSE
+    }
+    else{
+      group_formula <- TRUE
+    }
+  }
+  
+  if(group_formula){
+    # Split candidates list
+    candidates1 <- candidates %>%
+      filter(!is.na(Prior))
+    
+    # Group candidates
+    candidates2 <- candidates %>%
+      filter(is.na(Prior)) %>%
+      group_by(Formula,Charge) %>%
+      summarize(Name = paste(Name,collapse = "/"),
+                .groups = "drop")
+    
+    # Rejoin list
+    candidates <- candidates1 %>%
+      bind_rows(candidates2) %>%
+      arrange(Name)
+  }
+  
+  # Define design matrix
+  data <- sslamr_data(spectrum, 
+                      candidates = candidates,
+                      isotope_data = isotope_data,
+                      min_abundance = min_abundance,
+                      max_isotopes = Inf,
+                      skip_isotopes = 0,
+                      binning = binning,
+                      epsilon = epsilon, 
+                      min_mass_charge = min_mass_charge,
+                      max_mass_charge = max_mass_charge,
+                      prior_par = NULL,
+                      prescreen = prescreen,
+                      prescreen_prior = prescreen_prior,
+                      prescreen_weight = prescreen_weight,
+                      rounding = rounding,
+                      group_pattern = group_pattern,
+                      pattern_tol = pattern_tol,
+                      isoinfo = isoinfo,
+                      verbose = verbose)
+  
+  # Convert design matrix to data frame
+  design <- data$design |> 
+    pivot_longer(-Interval,
+                 names_to = "Name",
+                 values_to = "Proportion")
+  
+  # Extract rows with positive isotope proportions
+  design <- design |> 
+    filter(Proportion > 0) 
+  
+  # Add parent information
+  design <- design |> 
+    left_join(data$candidates |> select(Name = ID, Parent), by = "Name")
+  
+  # Extract observed counts
+  Count <- data$counts |> 
+    pull("Count")
+  
+  # Initialize output table
+  output <- tibble()
+  
+  continue <- TRUE
+  
+  while(continue){
+    
+    # Remove candidates whose parents are not yet in the model
+    if(nrow(output) == 0){
+      design1 <- design |> 
+        filter(Parent == Name)
+    }
+    else{
+      design1 <- design |> 
+        mutate(Keep = (Parent == Name) + (Parent %in% output$Name)) |>
+        filter(Keep > 0)
+    }
+   
+    
+    # Remove any candidates associated with all empty intervals
+    design1 <- design1 |> 
+      mutate(Counts = Count[Interval]) |> 
+      group_by(Name) |> 
+      mutate(Total = sum(Counts)) |> 
+      filter(Total > 0) |> 
+      select(-Total)
+    
+    # Fit models separately for each candidate
+    fits <- design1 |> 
+      mutate(Counts = Count[Interval]) |> 
+      group_by(Name) |> 
+      mutate(Total = sum(Count)) |> 
+      summarize(fit_single(first(Name), Proportion, Count[Interval]),
+                .groups = "drop") |> 
+      filter(Abundance > 0) |>
+      arrange(`p-value`)
+    
+    if(fits$`p-value`[1] < critical){
+      
+      # Save fit information
+      output <- output |> 
+        bind_rows(fits[1,])
+      
+      # Subtract from counts
+      name <- fits |> 
+        head(1) |> 
+        pull("Name")
+      
+      abund <- fits |> 
+        head(1) |> 
+        pull("Abundance")
+      
+      tmp <- design1 |> 
+        filter(Name == name)
+      
+      Count[tmp$Interval] <- (Count[tmp$Interval] - tmp$Proportion * abund) %>%
+        round() %>%
+        pmax(0)
+      
+      # Update design matrix  
+      design <- design |> 
+        filter(Name != name)
+    }
+    else{
+      continue <- FALSE
+    }
+  }
+  
+  # Group candidates with similar isotope patterns
+  if(group_pattern){
+    output_gpd <- output |> 
+      left_join(data$groups, by = "Name") |> 
+      group_by(Group_Name) |>
+      summarize(Abundance = sum(Abundance)) |> 
+      rename(Name = Group_Name)
+  }
+  
+  # Save output
+  write_xlsx(list(Separate = output,
+                  Grouped = output_gpd),
+             path = xlsx_out)
 }
